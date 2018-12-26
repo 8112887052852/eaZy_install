9d05a98dca2ff55a09ffe4d4dcceab6dce99848e
8112887052852 <tristan.roy@usherbrooke.ca>
2018-12-26 16:17:11 -0500

    Clearer output

--- t4_install.sh
+++ t4_install.sh
@@ -1,4 +1,36 @@
+echo 't4 eaZy_install'
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run script as root"
+  exit
+fi
+
+echo
+echo '***** Updating cached packages *****'
+echo
+
 apt update
+
+echo
+echo '***** Installing Aptitude *****'
+echo
+
 apt install -y aptitude
+
+echo
+echo "***** Updating Aptitude's package cache *****"
+echo
+
 aptitude update
+
+echo
+echo "***** Installing dev pac *****"
+echo
+
 aptitude install -y build-essential clang cmake git subversion vim
+
+echo
+echo '***** Upgrading packages *****'
+echo
+
+aptitude upgrade -y
