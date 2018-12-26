3d17ba35ca267c02be4fb18b452533bdd571ce79
8112887052852 <tristan.roy@usherbrooke.ca>
2018-12-26 16:35:57 -0500

    Added ssh-keygen

--- t4_install.sh
+++ t4_install.sh
@@ -1,36 +1,37 @@
 echo 't4 eaZy_install'
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run script as root"
-  exit
-fi
-
 echo
 echo '***** Updating cached packages *****'
 echo
 
-apt update
+sudo apt update
 
 echo
 echo '***** Installing Aptitude *****'
 echo
 
-apt install -y aptitude
+sudo apt install -y aptitude
 
 echo
 echo "***** Updating Aptitude's package cache *****"
 echo
 
-aptitude update
+sudo aptitude update
 
 echo
-echo "***** Installing dev pac *****"
+echo "***** Installing dev packages *****"
 echo
 
-aptitude install -y build-essential clang cmake git subversion vim
+sudo aptitude install -y build-essential clang cmake git subversion vim
 
 echo
 echo '***** Upgrading packages *****'
 echo
 
-aptitude upgrade -y
+sudo aptitude upgrade -y
+
+echo
+echo '***** Git configuration *****'
+echo
+
+ssh-keygen -t rsa -C "${USER}@$(hostname)"
